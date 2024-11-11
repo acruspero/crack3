ca647a573c12034213fa83a4593614b0417f5421
Andrei Cruspero <acruspero1@cs.sierracollege.edu>
2024-11-10 23:23:31 -0800

    bsearch and qsort

--- crack.c
+++ crack.c
@@ -11,6 +11,22 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
+// qsort compare function
+int strcomp(const void *a, const void *b)
+{
+    char **aa = (char **)a;
+    char **bb = (char **)b;
+    return strcmp(*aa, *bb);
+}
+
+// bsearch compare function
+int strsearch(const void *target, const void *elem)
+{
+    char *tt = (char *)target;
+    char **ee = (char **)elem;
+    
+    return strcmp(tt , *ee);
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,29 +36,60 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
+    // Read the hashes file into an array.
+    // Use the loadFile function from fileutil.c
     int size;
+
     //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
+    char **hashes = loadFileAA(argv[1], &size);
     
     // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    qsort(hashes, size, sizeof(char*), strcomp);
 
-    // TODO
+    // Open the password file for reading.
+    FILE *in = fopen(argv[2], "r");
+	if (!in)
+	{
+	    printf("Can't open password file for reading\n");
+	    exit(1);
+	}
+
     // For each password, hash it, then use the array search
     // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+    char password[PASS_LEN];
+    int count = 0;
 
-    // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+    while (fgets(password, PASS_LEN, in) != NULL)
+    {
+        // trim newline
+        char *nl = strrchr(password, '\n');
+        if (nl != NULL) *nl = '\0';
+        //printf("<%s>\n", password);
+
+        // hash each password
+        char *hashedPassword = md5(password, strlen(password));
+        //printf("%s\n", hashedPassword);
+
+        // linear search
+        //char *match = stringSearchAA(hashedPassword, hashes, size);
+
+        // CHALLENGE1: Use binary search instead of linear search.
+        char **match = bsearch(hashedPassword, hashes, size, sizeof(char*), strsearch);
+
+        // If there's a match, display the password and its hash
+        if (match)
+        {
+            // Ex: 5d41402abc4b2a76b9719d911017c592 hello
+            printf("%s %s\n", password, *match);
+            count++;
+        }
+        free(hashedPassword); // free malloc memory returned by md5
+    }
+
+    // Close the file and free memory
+    fclose(in);
+    freeAA(hashes, size);
+
+    // Display the number of hashes found.
+    printf("Hashes cracked: %d\n", count);
 }
